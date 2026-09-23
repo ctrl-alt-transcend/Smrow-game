753a605ccc99cc6f21647c9c0d8103fdeb8fa34e
Hayden <43919667+hdougoud@users.noreply.github.com>
2026-09-23 12:17:19 +0200

    FEAT(backend): Add validation pipe and interceptor
    - Added the class-validator and class-transformer packages
    - Configuration of global pipe validation in main.ts

--- app/backend/src/main.ts
+++ app/backend/src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
+
+/** @brief: Defines the application gateway. This is where the Nest instance starts up
+ */
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  /** @brief: A Pipe operates on a handler’s arguments (route parameters, body, query).
+  *           Its role is to validate or transform data before it reaches a controller.
+  */
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
+
