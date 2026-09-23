f4137eae2248559bf6099ab6d8fa6d52d234ca83
Matthieu Gillieron <gillieron.matthieu@ik.me>
2026-09-23 13:27:03 +0200

    feat: add app layout with routes

--- app/frontend/src/routes.tsx
+++ app/frontend/src/routes.tsx
@@ -1,13 +1,25 @@
 
 import { createBrowserRouter } from "react-router";
-import NotFoundPage from "./pages/NotFoundPage";
-import MainShell from './components/MainShell';
 
+import MainShell from './components/MainShell';
+import AppLayout from "./layouts/AppLayout"
+
+import  HomePage  from "./pages/HomePage"
+import ProfilePage from "./pages/ProfilePage";
+import StatsPage from "./pages/StatsPage";
+import NotFoundPage from "./pages/NotFoundPage";
 
 
 export const router = createBrowserRouter([
+	{
+		Component: AppLayout,
+		children:[
+			{ path: "home", Component: HomePage },
+			{ path: "profile", Component: ProfilePage },
+			{ path: "stats", Component: StatsPage },
+		],
+	},
 
-	{path: "/", Component: MainShell},
-	{path: "*", Component: NotFoundPage},
+	{ path: "/", Component: MainShell },
+	{ path: "*", Component: NotFoundPage },
 ]);
-
